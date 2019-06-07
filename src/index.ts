31dc18932fd75f47ddccfd66fd05744bc8e61be5
Haz <hazdiego@gmail.com>
2019-06-07 05:49:08 -0300

    chore: Initial setup for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-// @flow
-
 /**
  * This function says hello.
  * @param name Some name to say hello for.
  * @returns The hello.
  */
-const sayHello = (name: string = "Haz"): string => `Hello, ${name}!`;
+function sayHello(name = "Haz") {
+  return `Hello, ${name}!`;
+}
 
 export default sayHello;
